a7403403b88466c81349f54d988c7c011133bec6
xManticor <judsdeleonx@gmail.com>
2021-05-21 18:31:22 +0800

    Added content in routes.js. Also added controllers.

--- routes/routes.js
+++ routes/routes.js
@@ -3,63 +3,16 @@ const express = require('express');
 
 // import module `controller` from `../controllers/controller.js`
 const controller = require('../controllers/controller.js');
-
-// import module `signupController` from `../controllers/signupController.js`
-const signupController = require('../controllers/signupController.js');
-
-// import module `successController` from `../controllers/successController.js`
-const successController = require('../controllers/successController.js');
-
-// import module `profileController` from `../controllers/profileController.js`
-const profileController = require('../controllers/profileController.js');
+const user_controller = require('../controllers/userController');
+const product_controller = require('../controllers/productController');
+const login_controller = require('../controllers/loginController');
 
 const app = express();
 
-/*
-    execute function getFavicon()
-    defined in object `controller` in `../controllers/controller.js`
-    when a client sends an HTTP GET request for `/favicon.ico`
-*/
-app.get('/favicon.ico', controller.getFavicon);
+app.get('/', controller.get_index);
+app.get('/index', controller.get_index);
+app.get('/User', user_controller.getUser);
+app.get('/Product', product_controller.getProduct);
+app.get('/login', login_controller.getLogin);
 
-/*
-    execute function getIndex()
-    defined in object `controller` in `../controllers/controller.js`
-    when a client sends an HTTP GET request for `/`
-*/
-app.get('/', controller.getIndex);
-
-/*
-    execute function getSignUp()
-    defined in object `signupController` in `../controllers/signupController.js`
-    when a client sends an HTTP GET request for `/signup`
-*/
-app.get('/signup', signupController.getSignUp);
-
-/*
-    execute function postSignUp()
-    defined in object `signupController` in `../controllers/signupController.js`
-    when a client sends an HTTP POST request for `/signup`
-*/
-app.post('/signup', signupController.postSignUp);
-
-/*
-    execute function getSuccess()
-    defined in object `successController` in `../controllers/successController.js`
-    when a client sends an HTTP GET request for `/success`
-*/
-app.get('/success', successController.getSuccess);
-
-/*
-    execute function getProfile()
-    defined in object `profileController` in `../controllers/profileController.js`
-    when a client sends an HTTP GET request for `/profile/:idNum`
-    where `idNum` is a parameter
-*/
-app.get('/profile/:idNum', profileController.getProfile);
-
-/*
-    exports the object `app` (defined above)
-    when another script exports from this file
-*/
 module.exports = app;
